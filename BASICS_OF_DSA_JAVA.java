e0f0a1bacb87e3370338ba502f4b5f28b2754a9a
Harmehar Kaur <84430398+Harmehar-kaur@users.noreply.github.com>
2023-08-18 02:27:54 +0530

    Update BASICS_OF_DSA_JAVA.java

--- BASICS_OF_DSA_JAVA.java
+++ BASICS_OF_DSA_JAVA.java
@@ -583,3 +583,35 @@ class Solution {
         System.out.println(ans);
     }
 }
+//first index
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // no. of elements
+        int arr[] = new int[n];
+        // initializing array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt(); // element whose index to be searched
+        firstIndex(arr, n, x);
+    }
+
+    // Function for finding first occurrence
+    static void firstIndex(int[] arr, int n, int x) {
+        int first = -1;
+        for (int i = 0; i < n; i++) {
+            if (x == arr[i]) {
+                first = i;
+                break;
+            }
+        }
+
+        System.out.println(first);
+    }
+
+}
